172868bd736964d5fd59b0d398633652368c6960
leotalorac <leotalorac@unal.edu.co>
2020-09-27 01:08:29 -0500

    add responses

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Validator;
+
 use Illuminate\Http\Request;
 // models
 use App\Models\Course;
@@ -9,25 +11,39 @@ use App\Models\Course;
 
 class CourseController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $courses = Course::all();
         return $courses;
     }
-    public function create(Request $request){
-        $request->validate([
-            'name' =>'required',
-            'forum' =>'required'
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'forum' => 'required',
         ]);
-        Course::create([
-            "name"=>$request->name,
-            "forum"=>$request->forum
-        ]);
-        return [
-            "state"=>"ok",
-            "message"=>"course created"
-        ];
+        if ($validator->fails()) {
+            return response([
+                "state" => "error",
+                'errors'=>$validator->errors()
+            ],200);
+        }else{
+            Course::create([
+                "name" => $request->name,
+                "forum" => $request->forum
+            ]);
+            return response([
+                "state" => "ok",
+                "message" => "course created"
+            ],200);
+        }
     }
-    public function edit(){
-
+    public function edit(Request $request)
+    {
+        Course::whereId($request->id)->update($request->all());
+        return response([
+            "state" => "ok",
+            "message" => "course created"
+        ],200);
     }
 }
